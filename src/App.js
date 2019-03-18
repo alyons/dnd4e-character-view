39609e4d8d0c42879cf30304f21ab392c806cddf
Alexander Lyons <alexander.n.lyons@gmail.com>
2019-03-17 21:03:32 -0400

    feat: default style complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+import character from './images/017-paper.svg';
+import combat from './images/007-swords.svg';
+import equipment from './images/armor.svg';
+import inventory from './images/003-treasure.svg';
 import './App.css';
+import 'react-tabs/style/react-tabs.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Tabs>
+          <TabList>
+            <Tab><img src={character} className="App-tab-icon" alt="Character" /></Tab>
+            <Tab><img src={combat} className="App-tab-icon" alt="Combat" /></Tab>
+            <Tab><img src={equipment} className="App-tab-icon" alt="Equipment" /></Tab>
+            <Tab><img src={inventory} className="App-tab-icon" alt="Inventory" /></Tab>
+          </TabList>
+          <TabPanel>
+            <h2>Character Sheet</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Combat</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Equipment</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Inventory</h2>
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
