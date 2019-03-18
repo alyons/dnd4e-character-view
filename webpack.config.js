640998fcaefd34fd3b2e310749a47a60e7e546fb
Alexander Lyons <alexander.n.lyons@gmail.com>
2019-03-17 20:26:38 -0400

    feat: use webpack proper rather than react-scripts

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let plugins = [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'D&D 4E Character Viewer',
+        template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'style.[name].css'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+];
+
+module.exports = {
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(ico|json|svg)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', 'jsx']
+    }
+};
