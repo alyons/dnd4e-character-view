b5f1962d8f8632ac87762ecae4d099d14b7908b2
Alexander Lyons <alexander.n.lyons@gmail.com>
2019-03-25 22:48:28 -0400

    feat: add tests

--- src/actions/__tests__/vitalityActions.test.js
+++ src/actions/__tests__/vitalityActions.test.js
@@ -9,4 +9,33 @@ describe('vitality actions', () => {
         };
         expect(VitalityActions.heal(health)).toEqual(expectedAction);
     });
+
+    it('should create an action to take damage', () => {
+        const damage = 7;
+        const expectedAction = {
+            type: VitalityActions.TAKE_DAMAGE,
+            damage
+        };
+        expect(VitalityActions.takeDamage(damage)).toEqual(expectedAction);
+    });
+
+    it('should create an action to set temp hit hoints', () => {
+        const tempHitPoints = 5;
+        const expectedAction = {
+            type: VitalityActions.SET_TEMP_HIT_POINTS,
+            tempHitPoints
+        };
+        expect(VitalityActions.setTempHP(tempHitPoints)).toEqual(expectedAction);
+    });
+
+    it('should create an action to spend a healing surge', () => {
+        const regainHealth = true;
+        const extraHitPoints = 5;
+        const expectedAction = {
+            type: VitalityActions.SPEND_HEALING_SURGE,
+            regainHealth,
+            extraHitPoints
+        };
+        expect(VitalityActions.spendHealingSurge(regainHealth, extraHitPoints)).toEqual(expectedAction);
+    });
 });
